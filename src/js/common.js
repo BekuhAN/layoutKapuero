543f1447342548c2c6c570668019966cc74f67c4
BekuhAN <bekuh988@rambler.ru>
2018-04-21 01:47:19 +0300

    almost ready

--- src/js/common.js
+++ src/js/common.js
@@ -41,4 +41,18 @@ $(document).ready(function() {
 			menu.removeAttr('style');
 		}
 	}); 
+
+	$('a[href^="#b"]').bind('click.smoothscroll',function (e) {
+		e.preventDefault();
+ 
+		var target = this.hash,
+		$target = $(target);
+ 
+		$('html, body').stop().animate({
+			'scrollTop': $target.offset().top
+		}, 1500, 'swing', function () {
+			window.location.hash = target;
+		});
+	});
+	
 });
